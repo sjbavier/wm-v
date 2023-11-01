6f5ec7016521368a50ada4a53e8f49640068d86e
sjbavier <sjbavier@gmail.com>
2023-10-31 23:24:26 -0400

    adding configs

--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,17 @@ import { AuthProvider } from './components/auth/AuthProvider.tsx';
 import App from './App.tsx';
 
 import { MantineProvider } from '@mantine/core';
+// import { MantineConfig } from './mantine.config.ts';
+
+import { useToggle } from './hooks/useToggle.ts';
+const [isDark, toggleDark] = useToggle(false);
+const schema: string = isDark ? 'dark' : 'light';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
       <AuthProvider>
-        <MantineProvider>
+        <MantineProvider theme={{ colorScheme: schema }}>
           <App />
         </MantineProvider>
       </AuthProvider>
