14c58095af0595ed9ef2a6992588b75db7db6304
sjbavier <sjbavier@gmail.com>
2025-08-10 14:50:42 -0400

    syncing

--- src/components/music/music_grid/playlist-controls/PlayListControls.tsx
+++ src/components/music/music_grid/playlist-controls/PlayListControls.tsx
@@ -1,9 +1,15 @@
 import { alpha, darken, lighten, Menu } from '@mantine/core';
-import { IconMusicPlus, IconPlaylistAdd } from '@tabler/icons-react';
+// import type { MenuProps } from '@mantine/core'; // No longer needed if using styled-components for Menu
+import {
+  IconMusicPlus,
+  IconPlaylist,
+  IconPlaylistAdd
+} from '@tabler/icons-react';
 import styled from 'styled-components';
 import Render from '../../../render/Render';
 
 interface IPlayListControls {
+  // Assuming Song type is defined elsewhere
   song: Song;
   currentPlayList?: string;
   style: React.CSSProperties;
@@ -20,7 +26,7 @@ const PlayListControls = ({
           <IconMusicPlus onClick={(_e) => console.log(_e)} />
         </ControlButton>
       </Render>
-      <Menu>
+      <StyledMenu position="left" withArrow>
         <Menu.Target>
           <ControlButton>
             <IconPlaylistAdd />
@@ -30,10 +36,16 @@ const PlayListControls = ({
           <Menu.Item leftSection={<IconPlaylistAdd />}>
             Add to Current Playlist
           </Menu.Item>
-          <Menu.Item>Add to Existing Playlist</Menu.Item>
-          <Menu.Item>Create and Add to New Playlist</Menu.Item>
+          <Menu.Item
+            leftSection={<IconPlaylist />}
+            onClick={() =>
+              console.log(`Add to Playlist clicked for song: ${song.title}`)
+            } // Example handler
+          >
+            Add to Playlist
+          </Menu.Item>
         </Menu.Dropdown>
-      </Menu>
+      </StyledMenu>
     </PlayListControlsContainer>
   );
 };
@@ -42,6 +54,60 @@ const PlayListControlsContainer = styled.div`
   display: inline-flex;
   align-items: center;
   justify-content: end;
+`;
+
+const StyledMenu = styled(Menu)`
+  /*
+    IMPORTANT NOTE ON COLOR FUNCTIONS (alpha, lighten, darken):
+    Mantine's JS color functions (alpha, lighten, darken) expect actual color values
+    (e.g., hex codes, rgb strings) as input, not CSS variable strings like 'var(--mantine-color-green-5)'.
+    When used in styled-components like this: \`color: \${lighten('var(--mantine-color-green-5)', 0.1)};\`,
+    they will NOT produce the intended color modifications.
+    The styles below use CSS variables directly. For effects like opacity or specific shades,
+    you'd typically rely on pre-defined CSS variables for those states or use CSS-native properties.
+  */
+
+  // Styles for the dropdown panel itself
+  .mantine-Menu-dropdown {
+    border-width: 1px;
+    border-style: solid;
+    background: var(--shade-1); // Matches ControlButton background
+    // For border-color with opacity, you'd ideally have a CSS var like var(--mantine-color-green-6-alpha-50)
+    // or apply opacity to the whole element if that's acceptable.
+    // Using the direct color variable here:
+    border-color: var(--mantine-color-green-6);
+    color: var(
+      --mantine-color-green-5
+    ); // This will be the direct color, not lightened by JS
+  }
+
+  // Styles for individual menu items
+  .mantine-Menu-item {
+    color: var(--mantine-color-green-5); // Direct color
+
+    // Hover and focus states (Mantine uses data-hovered for keyboard nav)
+    &[data-hovered='true'],
+    &:hover {
+      // These will use the direct CSS variables, attempting to mirror ControlButton's variable choices
+      // The actual visual effect of lighten/darken from ControlButton's JS calls won't apply here.
+      color: var(
+        --mantine-color-green-5
+      ); // To make it lighter, you might use var(--mantine-color-green-4)
+      background-color: var(
+        --mantine-color-green-3
+      ); // This is green[3], not a darkened version of it.
+      // ControlButton's darken('var(--mantine-color-green-3)', 0.83)
+      // aims for a very dark color. To achieve that here, you'd
+      // need a CSS variable for that specific dark shade.
+    }
+  }
+
+  // Styles for the arrow
+  .mantine-Menu-arrow {
+    // Similar to dropdown border, opacity is tricky with direct CSS vars for borders.
+    border-color: var(--mantine-color-green-6);
+    background: var(--shade-1); // Match dropdown background
+  }
 `;
 
 const ControlButton = styled.div`
